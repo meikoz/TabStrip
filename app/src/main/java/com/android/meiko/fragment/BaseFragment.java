74680ad3f87bf77132f5f09f061e453389d5a982
zjlong <zhoujinlong@langtianhealth.com>
2016-05-26 10:49:07 +0800

    commit

---
+++ app/src/main/java/com/android/meiko/fragment/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.android.meiko.fragment;
+
+import android.app.Fragment;
+
+/**
+ * User:Shine
+ * Date:2015-10-29
+ * Description:
+ */
+public class BaseFragment extends Fragment {
+}
---
+++ app/src/main/java/com/android/meiko/fragment/PersonFragment.java
@@ -0,0 +1,23 @@
+package com.android.meiko.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.meiko.R;
+
+/**
+ * User:Shine
+ * Date:2015-10-20
+ * Description:
+ */
+public class PersonFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_person, container, false);
+    }
+}
